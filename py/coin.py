79b1ae2a84272b3087db3eb8eec95e8a3de051d2
Carter Carlson <cartercarlson94@gmail.com>
2019-03-02 15:23:59 -0700

    Add some basic methods to classes

--- py/coin.py
+++ py/coin.py
@@ -6,6 +6,24 @@ from datetime import datetime
 
 
 class Coin(object):
-    def __init__(self):
-        self.price = None # TODO: add difference between simulation price/current price
+
+    def __init__(self, coin, price=None):
+        self.coin = coin
+        self.init_price = None
+        self.init_units = None
+        self.current_price = None # TODO: add difference between simulation price/current price
+        self.current_units = None
+        self.market_val = None
+        self.transactions = {}
+        self.fees = None
+
+
+    def update_market_val(self):
         pass
+
+
+    def transact(self, side, units, price):
+        if side == 'buy':
+
+        # side == 'sold'
+        else:
--- py/portfolio.py
+++ py/portfolio.py
@@ -1,14 +1,33 @@
 import coin
 
+test = {'a':1, 'b':2,'c':3}
+
+
 class Portfolio(object):
 
-    def __init__(self, coins=None):
+    def __init__(self, coins=None, start_d_amt=None, transactions=None):
         self.cost = None
         self.market_val = None
         self.coins = {}
         self.pnl_unrealised_d_amt = None
         self.pnl_unrealised_pct = None
         self.pnl_realised_d_amt = None
-        self.fees = None
+        self.total_fees = None
+
+    def _add_coin(self, ticker, units, price, date=None):
+        if ticker not in self.coins:
+
+            self.coins[coin] = Coin(*args)
+            self._update_portfolio()
 
         pass
+
+    def _update_portfolio(self):
+        '''
+        Updates the market value of all coins in our portfolio
+        '''
+        for coin in self.coins:
+            # Update with current prices
+            self.coin.update_market_val()
+
+    def trade_coin(self, ticker, side, units)
