78087c9d0c439f1d4afffbb6cb7c77e32b5482eb
Carter <cartercarlson94@gmail.com>
2019-03-09 09:31:19 -0600

    Start improving README and continue work

--- py/exchange.py
+++ py/exchange.py
@@ -9,5 +9,14 @@ class Exchange(object):
 					                        'apiKey': api['apiKey'][0],
 					                        'secret': api['secret'][0]})
 
+		balance = self.binance.fetchBalance()['free']
+		self.coins_units = {coin: units
+		   for coin, units
+		   in balance.items()
+		   if units > 0.01
+		   and coin != 'ENJ'}
+
+
+
     def fetch_price(self):
         pass
