a8e9d40c8b286e25e4ec2c251658d8b1f9ea2cd1
Carter <cartercarlson94@gmail.com>
2019-03-15 21:26:05 -0500

    Add shell functions with parameters

---
+++ example/functions.py
@@ -0,0 +1,72 @@
+'''
+Methods
+
+update_balance(portfolio)
+update_market_val(portfolio)
+fetch_ticker(coin1, coin2)
+fetch_price(coin)
+calculate_units(coin, price, trade_value_in_usd)
+ticker_exists(coin1, coin2)
+trade(coin1, coin2, trade_value_in_usd)
+
+'''
+
+
+
+def update_market_val():
+'''
+- Updates all market value variables in portfolio
+- This is calculated before every rebalance to determine current coin weighting
+'''
+
+
+def update_balance(portfolio):
+'''
+Purpose:
+
+Variables needed from data.json:
+
+'''
+    pass
+
+
+def update_market_val(portfolio):
+'''
+Purpose:
+'''
+    pass
+
+
+def fetch_ticker(coin1, coin2):
+'''
+Purpose:
+'''
+    pass
+
+
+def fetch_price(coin):
+'''
+Purpose:
+'''
+    pass
+
+
+def calculate_units(coin, price, trade_value_in_usd):
+'''
+Purpose:
+'''
+    pass
+
+
+def ticker_exists(coin1, coin2):
+'''
+Purpose:
+'''
+    pass
+
+
+def trade(coin1, coin2, trade_value_in_usd):
+'''
+Purpose:
+'''
+    pass
