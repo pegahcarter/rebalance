ba56b7d065291d7762027400983acdcc469e9ba6
Carter Carlson <carlfarterson@gmail.com>
2019-06-06 13:06:38 -0500

    Start from scratch using transaction-log as reference

--- py/portfolio.py
+++ py/portfolio.py
@@ -1,32 +1,15 @@
-import ccxt
-import pandas as pd
-from coin import Coin
+from transactions import Transactions
+
 
 class Portfolio:
 
-    def __init__(self, **kwargs):
-        self.transactions = []
-        coins = kwargs.pop('coins')
-        for key, val in kwargs.items():
-            setattr(self, key, val)
+    fee_rate = fee_rate
+    slippage_rate = slippage_rate
+    
+    def __init__(self, simulated=False, coins=None):
+        self.simulated = simulated
+        self.transactions = Transactions(simulated, coins)
 
-        self.coins = {}
-        for coin in coins:
-            self._add_coin(coin)
-            self.transactions.append({
-                'id': len(self.transactions) + 1,
-                'date': self.prices['timestamp'].at[0],
-                'buy_or_sell': 'buy',
-                'price': self.coins[coin]['price'],
-                'units': self.coins[coin]['units']
-            })
-
-    # This adds the initial purchase of coin
-    def _add_coin(self, coin):
-        price = self.prices[coin].at[0]
-        units = self.start_amt/price
-        self.coins[coin] = {
-            'price': price,
-            'units': units,
-            'market_val': price * units
-        }
+    def summarize(self):
+        # Add summary
+        self.summary = {}
---
+++ py/transactions.py
@@ -0,0 +1,13 @@
+import json
+
+
+class Transactions:
+    file = 'transactions.json'
+    def __init__(self, simulated, coins):
+        try:
+            with open(self.file, 'r') as f:
+                transactions = json.load(f)
+        except:
+            transactions = []
+
+        return transactions
