cdcbdcefc50d3bc2327a56449633ece0547d9606
Carter Carlson <carlfarterson@gmail.com>
2019-06-24 16:40:52 -0500

    Remove code to written for testing backtest

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import transactions from '../assets/transactions.json';
 
-const columns = []
+const columns = [
+  {
+    Header: 'Trade ID',
+    accessor: 'id',
+    width: 100
+  },
+  {
+    Header: 'Date',
+    accessor: 'date'
+  },
+  {
+    Header: 'Coin',
+    accessor: 'coin'
+  },
+  {
+    Header: 'Side',
+    accessor: 'side'
+  },
+  {
+    Header: 'Units',
+    accessor: 'units'
+  },
+  {
+    Header: 'Cost',
+    accessor: 'cost'
+  },
+  {
+    Header: 'Previous Units',
+    accessor: 'prev_units'
+  },
+  {
+    Header: 'Cumulative Units',
+    accessor: 'cum_units'
+  },
+  {
+    Header: 'Previous Cost',
+    accessor: 'prev_cost'
+  },
+  {
+    Header: 'Cumulative Cost',
+    accessor: 'cum_cost'
+  },
+  {
+    Header: 'Fees',
+    accessor: 'fees'
+  }
+]
+
 
 export default class Transactions extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      tx_count: 1
-    };
   }
 
   render() {
     return (
-      <div>Transactions</div>
+      <div>
+        <ReactTable
+          columns={columns}
+          data={transactions}
+        >
+        </ReactTable>
+      </div>
     )
   }
 }
