23f046df164f676fa6553e2548cac5eef3bc3be1
Carter Carlson <carlfarterson@gmail.com>
2019-06-25 13:56:55 -0500

    Start creating django components

---
+++ interface/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
