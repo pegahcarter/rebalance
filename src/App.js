b3d81a05bef40ffb58d61c9fb66e6c53c65bada3
Carter Carlson <carlfarterson@gmail.com>
2019-07-23 09:22:21 -0500

    Update README.md

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ export default class App extends Component {
     return (
       <div style={{ marginLeft: '10%', marginRight: '10%' , marginTop: 100}}>
         <div>
-          <Coins />
           <Graph />
           <Simulate submit={this.submitSimulate} />
         </div>
