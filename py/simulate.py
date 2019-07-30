5cca119541307b649236499fd58d5a8866616926
Carter Carlson <carlfarterson@gmail.com>
2019-07-29 21:19:19 -0500

    Succesfully rendered basic chart with victoryjs

--- py/simulate.py
+++ py/simulate.py
@@ -12,14 +12,15 @@ portfolio = Portfolio(COINS, PORTFOLIO_START_VALUE)
 for i in range(1, len(portfolio.hist_prices)):
     if i % INTERVAL == 0:
         portfolio = rebalance.run(portfolio, i)
-    # Append hodl total value
-    df_rebalanced.append(np.dot(portfolio.units, portfolio.hist_prices[i]))
-    # Append rebalanced total value
-    df_hodl.append(np.dot(portfolio.start_units, portfolio.hist_prices[i]))
+    if i % 24 == 0:
+        # Append hodl total value
+        df_rebalanced.append(np.dot(portfolio.units, portfolio.hist_prices[i]))
+        # Append rebalanced total value
+        df_hodl.append(np.dot(portfolio.start_units, portfolio.hist_prices[i]))
 
 
 sim_results = pd.DataFrame(zip(portfolio.dates, df_hodl, df_rebalanced), columns=['date', 'hodl', 'rebalanced'])
-sim_results.to_json(SIM_RESULTS_FILE, orient='records')
+sim_results.to_json(SIM_RESULTS_FILE)
 
 
 portfolio.summarize()
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,36 +1,34 @@
 import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+import {VictoryChart, VictoryLine} from 'victory';
 import results from '../assets/sim_results.json';
+
+// data={[
+//   { x: 1, y: 2 },
+//   { x: 2, y: 4},
+//   { x: 3, y: 8}
+// ]}
+
+var data = []
+for (var i in results.hodl) {
+  data.push({
+    y: results.hodl[i]
+  });
+}
+
+console.log(data);
+
 
 
 export default class Chart extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartData: {
-        labels: [],
-        datasets: [{
-          label: 'Rebalanced',
-          data: results.rebalanced
-        }]
-      }
-    }
-  }
-
   render() {
     return (
-      <div className='chart'>
-        <Line
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: true,
-              text: 'Rebalanced vs. Non-Rebalanced',
-              fontSize:25
-            }
-          }}
-        />
+      <div>
+        <VictoryChart>
+          <VictoryLine
+            data={data}
+          />
+        </VictoryChart>
       </div>
     );
   }
